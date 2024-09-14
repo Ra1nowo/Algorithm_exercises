d91bea7ef87c5e8f466277467e3e23c9e9a49ec3
听见心雨 <lyrufeng@qq.com>
2024-09-14 19:23:15 +0800

    单调栈+递推+贪心

---
+++ pracitice/单调栈/132模式(单调栈找右边最大的元素).cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+const int N = 2e5 + 10, INF = -1e9;
+int a[N];
+stack<int> stk;
+
+bool is132Pattern(int n) {
+    int leftMin[n], numsk = INF;
+    fill(leftMin,leftMin+n,INF);
+    for (int i = 1; i < n; ++i) leftMin[i] = min(leftMin[i-1],a[i-1]);
+    for (int j = n-1; j >= 0; --j) {
+        while (!stk.empty() && stk.top() < a[j]) {
+            numsk = stk.top();
+            stk.pop();
+        }
+        if (numsk > a[j]) return true;
+        stk.push(a[j]);
+    }
+    return false;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; ++i) cin >> a[i];
+    string res = is132Pattern(n)? "Yes" : "No";
+    cout << res << endl;
+    return 0;
+}
